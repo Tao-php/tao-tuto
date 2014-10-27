ff6bb598913eaf1c21d076e1f83623caa0f16732
Vincent Garnier <forxer@gmail.com>
2014-10-27 10:34:31 +0100

    avancement contact

--- Application/Controllers/Contact.php
+++ Application/Controllers/Contact.php
@@ -3,8 +3,45 @@ namespace Application\Controllers;
 
 class Contact extends BaseController
 {
+	protected $email;
+	protected $subject;
+	protected $message;
+
 	public function form()
 	{
-		return $this->render('Contact');
+		return $this->render('Contact', [
+			'email' => $this->email,
+			'subject' => $this->subject,
+			'message' => $this->message
+		]);
+	}
+
+	public function process()
+	{
+		$this->email = $this->app['request']->request->get('email');
+		$this->subject = $this->app['request']->request->get('subject', 'email depuis la page contact');
+		$this->message = $this->app['request']->request->get('message');
+
+		if (empty($this->email)) {
+			$this->app['instantMessages']->error('Vous devez saisir une adresse email.');
+		}
+		elseif (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+			$this->app['instantMessages']->error('Vous devez saisir une adresse email valide.');
+		}
+
+		if (empty($this->message)) {
+			$this->app['instantMessages']->error('Vous devez saisir un message.');
+		}
+
+		# si on as une erreur, on ré-affiche le formulaire
+		if ($this->app['instantMessages']->hasError()) {
+			return $this->form();
+		}
+
+		// envoi de l'email ...
+
+		$this->app['flashMessages']->success('Votre email a été envoyé.');
+
+		return $this->redirectToRoute('contact');
 	}
 }
--- Application/Views/Contact.php
+++ Application/Views/Contact.php
@@ -4,13 +4,13 @@
 
 <form action="" method="post">
 	<p><label for="mail">Adresse email</label><br>
-	<input name="mail" id="mail" type="email" size="50" maxlength="255" value=""></p>
+	<input name="email" id="email" type="text" size="50" maxlength="255" value="<?php echo $view->e($email) ?>"></p>
 
 	<p><label for="subject">Sujet</label><br>
-	<input name="subject" id="subject" type="text" size="50" maxlength="255" value=""></p>
+	<input name="subject" id="subject" type="text" size="50" maxlength="255" value="<?php echo $view->e($subject) ?>"></p>
 
 	<p><label for="message">Message</label><br>
-	<textarea name="message" id="message" cols="37" rows="7"></textarea></p>
+	<textarea name="message" id="message" cols="37" rows="7"><?php echo $view->e($message) ?></textarea></p>
 
 	<p><input type="submit" class="submit" value="envoyer"></p>
 </form>
